a38269c86b8227371c72bc56b0427658e2cded15
Luis Falero <lfalero@redhat.com>
2022-11-21 23:45:46 -0500

    Demo

---
+++ src/main/java/com/lfalero/amqp/AmqpApplication.java
@@ -0,0 +1,36 @@
+package com.lfalero.amqp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+
+@EnableJms
+@SpringBootApplication
+public class AmqpApplication implements CommandLineRunner {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		sendMessage("Hello World!");
+	}
+
+	public void sendMessage(String text) {
+		System.out.println(String.format("Sending '%s'", text));
+		this.jmsTemplate.convertAndSend("example", text);
+	}
+
+	@JmsListener(destination = "example")
+	public void receiveMessage(String text) {
+		System.out.println(String.format("Received '%s'", text));
+	}
+}
